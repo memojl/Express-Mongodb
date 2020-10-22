f34e490753bc76e9675bd40adbd7ab22ce9d2da9
Memo Jimenez <memojl08@gmail.com>
2020-10-22 11:21:26 -0500

    update secciones

--- router/RutasWeb.js
+++ router/RutasWeb.js
@@ -5,8 +5,16 @@ router.get('/',(req, res)=>{
   res.render('index',{titulo: 'Mi titulo dinamico'});
 });
 
+router.get('/nosotros',(req, res)=>{
+  res.render('nosotros',{titulo: 'Nosotros'});
+});
+
 router.get('/servicios',(req, res)=>{
-  res.render('servicios',{titulo: 'Este es un mensaje dinamico de la pagina de servicios'});
+  res.render('servicios',{titulo: 'Servicios'});
+});
+
+router.get('/contacto',(req, res)=>{
+  res.render('contacto',{titulo: 'Contacto'});
 });
 
 module.exports = router;
